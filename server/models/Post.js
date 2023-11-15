1fffeafb48c9dab819bddd83695bf0f3c35f5973
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2023-11-14 21:17:56 -0600

    my blog

---
+++ server/models/Post.js
@@ -0,0 +1,69 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const postSchema = new Schema({
+  postTitle: {
+    type: String,
+    required: "You need to give a title!",
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  postImage: {
+    type: String,
+    trim: true,
+  },
+  postText: {
+    type: String,
+    required: "You need to leave a post!",
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  postAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        minlength: 1,
+        maxlength: 280,
+      },
+      commentAuthor: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+  likes: [
+    {
+      likeText: {
+        type: String,
+      },
+      likeAuthor: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+});
+
+const Post = model("Post", postSchema);
+
+module.exports = Post;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,68 @@
+// Defining GraphQL type definitions
+const typeDefs = `
+  # Defining a "User" type with it values
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    posts: [Post]
+  }
+
+  # Defining "Post" type with it values
+  type Post {
+    _id: ID!
+    postTitle: String
+    postImage: String
+    postText: String
+    postAuthor: String
+    createdAt: String
+    comments: [Comment]
+    likes: [Like]
+  }
+
+  # Defining "Comment" type with it values
+  type Comment {
+    _id: ID
+    commentText: String!
+    commentAuthor: String!
+    createdAt: String
+  }
+
+    # Defining "Like" type with it values
+  type Like {
+    _id: ID
+    likeText: String!
+    likeAuthor: String!
+    createdAt: String
+  }
+
+  # Defining an "Auth" type with it values
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  # Defining "Query" type with it values
+  type Query {
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(postId: ID!): Post
+    me: User
+  }
+
+  # Defining "Mutation" types
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addPost(postTitle: String!, postImage: String, postText: String!): Post
+    addComment(postId: ID!, commentText: String!): Post
+    removePost(postId: ID!): Post
+    removeComment(postId: ID!, commentId: ID!): Post
+    addLike(postID: ID!): Post
+    removeLike(postID: ID!): Post
+  }
+`;
+
+// Export the "type definitions"
+module.exports = typeDefs;
